1b01ce9df50460e6935574c67fa47a77147e3428
Juwami <sean.nguyenf@yahoo.com>
2019-09-09 14:41:25 -0400

    add connection for mongodb, create model folder with GoogleBooks model

--- server.js
+++ server.js
@@ -2,16 +2,63 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Use the public
+app.use(express.static("public"));
+const db = require("./models");
+
+mongoose.connect("mongodb://localhost/googlebooks", {
+  useNewUrlParser: true
+});
+
 // Define API routes here
+// /api/books (get) - Should return all saved books as JSON.
+// /api/books (post) - Will be used to save a new book to the database.
+// /api/books/:id (delete) - Will be used to delete a book from the database by Mongo _id.
+
+app.get("/api/books", (req, res) => {
+  db.GoogleBook.find({})
+    .then(books => {
+      res.json(books)
+    })
+    .catch(err => {
+      res.json(err)
+    })
+})
+
+app.post("/api/books", (req, res) => {
+  db.GoogleBook.create({})
+    .then(books => {
+      // code to add to json
+    })
+    .catch(err => {
+      res.json(err)
+    })
+})
+
+app.post("/api/books/:id", (req, res) => {
+  db.GoogleBook.remove({})
+    .then(
+      // code to delete books
+    )
+    .catch(err => {
+      console.log(err)
+      throw err
+    })
+})
+
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
