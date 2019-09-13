8a8d4a9a6c3dc62a254f84dfb8c29d879dc532a3
Juwami <sean.nguyenf@yahoo.com>
2019-09-13 17:03:45 -0400

    edit display for brand to display-4 while default size for the links

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 function Navbar() {
 	return (
 		<div>
-			<nav className="navbar navbar-dark bg-dark display-4">
+			<nav className="navbar navbar-dark bg-dark">
                 <a href="/" className="navbar-brand">
-                    <h3 className="">Google Books</h3>
+                    <h3 className="display-4">Google Books</h3>
                 </a>
+				<ul className="nav">
+					<li className="nav-item">
+						<a href="/Search" className="nav-link active">Search</a>
+					</li>
+					<li className="nav-item">
+						<a href="/Saved" className ="nav-link active">Saved</a>
+					</li>
+				</ul>
             </nav>
 		</div>
 	);
